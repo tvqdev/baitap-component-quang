ff0d80ef8eda74c82845b2d0dd0623866228d68e
Quang <tvq.dev@gmail.com>
2022-02-25 20:11:45 +0700

    fix class

--- src/Components/HomeComponent/FooterComponent.js
+++ src/Components/HomeComponent/FooterComponent.js
@@ -3,9 +3,10 @@ import React, { Component } from 'react'
 export default class FooterComponent extends Component {
      render() {
           return (
-               <footer class="py-5 bg-dark">
-                    <div class="container"><p class="m-0 text-center text-white">Copyright &copy; Your Website 2021</p></div>
+               <footer className="py-5 bg-dark">
+                    <div className="container"><p className="m-0 text-center text-white">Copyright © Your Website 2021</p></div>
                </footer>
+
           )
      }
 }
